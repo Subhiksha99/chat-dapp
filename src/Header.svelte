85ed95492fd02fc5426fd568b2930775cbdb2e55
subhiksha <subhikshamaheshwari@gmail.com>
2022-07-04 23:24:02 +0530

    initial-commit

---
+++ src/Header.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { username, user } from './user';
+
+  function signout() {
+    user.leave();
+    username.set('');
+  }
+</script>
+
+<header>
+<h1><img src="/image/icons8-chat-48.png"/></h1>
+  {#if $username}
+    <div class="user-bio">
+      
+      <span>Hello <strong>{$username}</strong></span>
+      <img src={`https://avatars.dicebear.com/api/initials/${$username}.svg`} alt="avatar" /> 
+    </div>
+
+    <button class="signout-button" on:click={signout}>Sign Out</button>
+
+    {:else}
+
+      <h3>Messenger</h3>
+  {/if}
+</header>
+
